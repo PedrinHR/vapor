52033915e62440dc41fd017fa4f8ff627bda87b6
PedroHR <pedro.renosto@hotmail.com>
2022-12-01 22:31:58 -0300

    feito a função de obterHoraAtual, ou seja função para receber a data e hora

--- painel/assets/scripts.js
+++ painel/assets/scripts.js
@@ -27,3 +27,33 @@ function removerElementoPorId(idElemento) {
 
 
 }
+function obterHoraAtual() {
+    const data = new Date()
+    let hora = data.getHours()
+    let minutos = data.getMinutes()
+    let segundos = data.getSeconds()
+    let dia = data.getDate()
+    let mes = data.getMonth()
+    let ano = data.getFullYear()
+  
+  if(hora < 10){
+    hora = "0" + hora
+  }
+  if(minutos < 10){
+    minutos = "0" + minutos
+  }
+  if(segundos < 10){
+    segundos = "0" + segundos
+  }
+  if(dia < 10 ){
+    dia = "0" + dia
+  }
+  if(mes < 10){
+    mes = "0" + mes
+  }
+    let dataAtual = dia + "/" + (mes+1) + "/" + ano + "-" + hora + ":" + ":" + minutos + ":" + segundos
+  
+    return dataAtual
+  }
+  let dataHora = obterHoraAtual()
+  console.log(dataHora)
